9c400f3d4c71885266ff19bb21e0b54620b9c23c
Andy S <andy.soelistio@gmail.com>
2017-09-06 23:30:03 +0700

    feat: Edited module crimemap and github practice

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
-import sqlite3
 from db_helper import DB_Helper
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    data = DB_Helper.get_all()
-    return render_template('home.html',data=data)
+    crimes = DB_Helper.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template('home.html',crimes=crimes)
 
-@app.route('/add', methods=['POST'])
-def add():
-    data = request.form.get('userinput')
-
-    DB_Helper.insert_item(data)
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB_Helper.add_crime(category, date, latitude, longitude, description)
     return home()
 
 @app.route('/clear')
@@ -22,20 +26,5 @@ def clear_db():
     return home()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
-    app.run(port=5002, debug=True)
+    app.run(port=5000, debug=True)
--- crimemap/db_helper.py
+++ crimemap/db_helper.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 class DB_Helper():
 
@@ -22,10 +23,43 @@ class DB_Helper():
         return data
 
     @classmethod
+    def get_all_crimes(cls):
+        connection = sqlite3.connect('crimemap.db')
+        cursor = connection.cursor()
+        query = "SELECT latitude,longitude,date,category,description FROM crimes"
+        result = cursor.execute(query)
+        data = result.fetchall()
+        named_crimes = []
+        for crime in data:
+            named_crime = {
+                'latitude': crime[0],
+                'longitude': crime[1],
+                 #'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+                 'date': crime[2],
+                'category': crime[3],
+                'description': crime[4]
+            }
+            named_crimes.append(named_crime)
+
+        connection.close()
+        return named_crimes
+
+
+    @classmethod
     def clear(cls):
         connection = sqlite3.connect('crimemap.db')
         cursor = connection.cursor()
         query = "DELETE FROM crimes"
         cursor.execute(query)
+        connection.commit()
+        connection.close()
+
+    @classmethod
+    def add_crime(cls, category, date, latitude, longitude, desc):
+        connection = sqlite3.connect('crimemap.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO crimes (category, date, latitude, longitude, description) \
+                    VALUES(?,?,?,?,?)"
+        cursor.execute(query,(category,date,latitude,longitude,desc))
         connection.commit()
         connection.close()
